0e6dfa9f8216e76c1902dfd5711a15a101909b32
Charlie Lowe <clowe@salisbury6c.ac.uk>
2019-11-22 11:00:30 +0000

    hubjnk

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,24 +1,26 @@
 var weather = document.getElementsByClassName("weather");
+var page = document.getElementsByTagName("body")[0];
+var pageClass = parseInt(page.className);
 window.addEventListener("load", function() {
   getLocation();
 });
 
 async function getWeather(lat, long) {
   var response = await fetch(
-    "http://api.weatherunlocked.com/api/current/" +
+    "http://api.weatherunlocked.com/api/forecast/" +
       lat +
       "," +
       long +
       "?app_id=9e946863&app_key=e08ee008e50d82b421467fee14ab421b"
   );  
   var data = await response.json();
-  weather[1].setAttribute("src", "../resources/images/icons/" + data.wx_icon);
-  weather[0].innerHTML = "Weather = " + data.wx_desc;
-  weather[2].innerHTML = "Temperature = " + data.temp_c;
-  weather[3].innerHTML = "Wind Speed = " + data.windspd_kmh+"km/h";
-  weather[4].innerHTML = "Wind Direction = " + data.winddir_deg+" degrees";
-  
   console.log(data);
+
+  weather[0].innerHTML = "Min Temp = " + data.Days[pageClass].temp_min_c+" °C";
+  weather[1].innerHTML = "Max Temp = " + data.Days[pageClass].temp_max_c+" °C";
+  weather[2].innerHTML = "Max Wind Speed = " + data.Days[pageClass].windspd_max_mph+"mph";
+  weather[3].innerHTML = "Wind Direction = " + data.Days[pageClass].winddir_deg+" degrees";
+
 
 }
 
