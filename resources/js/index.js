de5172e04f468ed6599ae3bbad52de57d1e36d06
Charlie Lowe <clowe@salisbury6c.ac.uk>
2019-11-22 10:32:29 +0000

    tygujiokp

--- resources/js/index.js
+++ resources/js/index.js
@@ -10,12 +10,16 @@ async function getWeather(lat, long) {
       "," +
       long +
       "?app_id=9e946863&app_key=e08ee008e50d82b421467fee14ab421b"
-  );
+  );  
   var data = await response.json();
-  weather[0].setAttribute("src", "../resources/images/icons/" + data.wx_icon);
-  weather[1].innerHTML = "Weather = " + data.wx_desc;
+  weather[1].setAttribute("src", "../resources/images/icons/" + data.wx_icon);
+  weather[0].innerHTML = "Weather = " + data.wx_desc;
   weather[2].innerHTML = "Temperature = " + data.temp_c;
+  weather[3].innerHTML = "Wind Speed = " + data.windspd_kmh+"km/h";
+  weather[4].innerHTML = "Wind Direction = " + data.winddir_deg+" degrees";
+  
   console.log(data);
+
 }
 
 function getLocation() {
@@ -29,3 +33,4 @@ function showPosition(position) {
   var long = position.coords.longitude;
   getWeather(lat, long);
 }
+
