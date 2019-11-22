13c80cffdff46b001650509aa6c08c0b88e46dda
Tom Newby <tnewby@salisbury6c.ac.uk>
2019-11-22 13:41:58 +0000

    gsfs

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,20 @@
 var weather = document.getElementsByClassName("weather");
 var page = document.getElementsByTagName("body")[0];
+var today = document.getElementsByClassName("today")[0];
+
+var d = new Date();
+var days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+today.innerHTML = "Current Weather - Today - " + days[d.getDay()];
+
 var pageClass = parseInt(page.className);
 window.addEventListener("load", function() {
   getLocation();
@@ -12,16 +27,32 @@ async function getWeather(lat, long) {
       "," +
       long +
       "?app_id=9e946863&app_key=e08ee008e50d82b421467fee14ab421b"
-  );  
+  );
   var data = await response.json();
   console.log(data);
 
-  weather[0].innerHTML = "Min Temp = " + data.Days[pageClass].temp_min_c+" °C";
-  weather[1].innerHTML = "Max Temp = " + data.Days[pageClass].temp_max_c+" °C";
-  weather[2].innerHTML = "Max Wind Speed = " + data.Days[pageClass].windspd_max_mph+"mph";
-  weather[3].innerHTML = "Wind Direction = " + data.Days[pageClass].winddir_deg+" degrees";
+  weather[4].innerHTML =
+    "Min Temp = " + data.Days[pageClass].temp_min_c + " °C";
+  weather[5].innerHTML =
+    "Max Temp = " + data.Days[pageClass].temp_max_c + " °C";
+  weather[7].innerHTML =
+    "Max Wind Speed = " + data.Days[pageClass].windspd_max_mph + "mph";
+}
 
-
+async function getCurrentWeather(lat, long) {
+  var response = await fetch(
+    "http://api.weatherunlocked.com/api/current/" +
+      lat +
+      "," +
+      long +
+      "?app_id=9e946863&app_key=e08ee008e50d82b421467fee14ab421b"
+  );
+  var current = await response.json();
+  console.log(current);
+  weather[0].innerHTML = "Weather = " + current.wx_desc;
+  weather[6].innerHTML = "Current Wind Speed = " + current.windspd_mph + "mph";
+  weather[2].innerHTML = "Current Temp = " + current.temp_c + " °C";
+  weather[8].innerHTML = "Wind Direction = " + current.winddir_deg + " degrees";
 }
 
 function getLocation() {
@@ -33,6 +64,6 @@ function getLocation() {
 function showPosition(position) {
   var lat = position.coords.latitude;
   var long = position.coords.longitude;
+  getCurrentWeather(lat, long);
   getWeather(lat, long);
 }
-
